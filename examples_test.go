e374c5ea2e4be206e824bbf29f438648f28684ca
rwxrob <rob@rwx.gg>
2021-07-12 01:11:49 -0400

    Discard interface, struct instead, much better

--- examples_test.go
+++ examples_test.go
@@ -6,71 +6,35 @@ import (
 	"github.com/rwxrob/tree-go"
 )
 
-func ExampleNewNode() {
-
-	node := tree.NewNode()
-	node.Kind = 2
-	node.Value = "something"
-
-	fmt.Printf(
-		"Type: %T\nKind: %v\nValue: %v",
-		node, node.Kind, node.Value,
-	)
-
-	// Output:
-	// Type: *tree.node
-	// Kind: 2
-	// Value: something
-
-}
-
 func ExampleNode() {
 
-	r := tree.NewNode()
-	r.Kind = 1
-	r.Value = "foo"
-
-	c := tree.NewNode()
-	c.Kind = 2
-	c.Value = "bar"
-
-	o := tree.NewNode()
-	o.Kind = 2
-	o.Value = "other"
-
-	l := tree.NewNode()
-	l.Kind = 2
-	l.Value = "last"
-
-	r.AddChild(c)
-	r.AddChild(o)
-	r.AddChild(l)
-
-	newFirst := tree.NewNode()
-	newFirst.Kind = 3
-	newFirst.Value = "FIRST"
+	r := &tree.Node{Kind: 1, Value: "foo"}
+	c := &tree.Node{Kind: 2, Value: "bar"}
+	o := &tree.Node{Kind: 2, Value: "other"}
+	l := &tree.Node{Kind: 2, Value: "last"}
+	r.Add(c, o, l)
 
 	fmt.Printf(`
-Root:  %T %v %v %v %v %v
-Child: %T %v %v %v %v %v
-Other: %T %v %v %v %v %v
-Last:  %T %v %v %v %v %v
+Root:  %v %v %v %v %v
+Child: %v %v %v %v %v
+Other: %v %v %v %v %v
+Last:  %v %v %v %v %v
 Number of Children: %v
 First Child: %v
 Last Child:  %v
 `,
-		r, r.Kind, r.Value, r.Parent(), r.Prev(), r.Next(),
-		c, c.Kind, c.Value, c.Parent().Value, c.Prev(), c.Next().Value,
-		o, o.Kind, o.Value, o.Parent().Value, o.Prev().Value, o.Next().Value,
-		l, l.Kind, l.Value, l.Parent().Value, l.Prev().Value, l.Next(),
-		len(r.Children()), r.ChildFirst().Value, r.ChildLast().Value,
+		r.Kind, r.Value, r.Parent, r.Prev, r.Next,
+		c.Kind, c.Value, c.Parent.Value, c.Prev, c.Next.Value,
+		o.Kind, o.Value, o.Parent.Value, o.Prev.Value, o.Next.Value,
+		l.Kind, l.Value, l.Parent.Value, l.Prev.Value, l.Next,
+		len(r.Children()), r.FirstChild.Value, r.LastChild.Value,
 	)
 
 	// Output:
-	// Root:  *tree.node 1 foo <nil> <nil> <nil>
-	// Child: *tree.node 2 bar foo <nil> other
-	// Other: *tree.node 2 other foo bar last
-	// Last:  *tree.node 2 last foo other <nil>
+	// Root:  1 foo <nil> <nil> <nil>
+	// Child: 2 bar foo <nil> other
+	// Other: 2 other foo bar last
+	// Last:  2 last foo other <nil>
 	// Number of Children: 3
 	// First Child: bar
 	// Last Child:  last
--- tree.go
+++ tree.go
@@ -1,68 +1,44 @@
 package tree
 
-// AddChild fulfills the Node interface by adding the specified Node as
-// a new child. See NewNode for more about how to create a Node to be
-// added.
-
-// Children must return a slice of Nodes even if there are no children.
-
-type Node interface {
-	Parent() Node
-	Prev() Node
-	Next() Node
-	ChildFirst() Node
-	ChildLast() Node
-	AddChild(Node)
-	Children() []Node
-	InsertBefore(Node)
-	InsertAfter(Node)
-	Delete()
+type Node struct {
+	Kind       int
+	Value      interface{}
+	Parent     *Node
+	Prev       *Node
+	Next       *Node
+	FirstChild *Node
+	LastChild  *Node
 }
 
-type node struct {
-	Kind  int
-	Value interface{}
-
-	parent *node
-	prev   *node
-	next   *node
-	first  *node
-	last   *node
-}
-
-func (n *node) Parent() *node     { return n.parent }
-func (n *node) Prev() *node       { return n.prev }
-func (n *node) Next() *node       { return n.next }
-func (n *node) ChildFirst() *node { return n.first }
-func (n *node) ChildLast() *node  { return n.last }
-
-func (n *node) Children() []*node {
-	chs := []*node{}
-	if n.first == nil {
-		return chs
+func (n *Node) Children() []*Node {
+	children := []*Node{}
+	if n.FirstChild == nil {
+		return children
 	}
-	for cur := n.first; cur != nil; cur = cur.next {
-		chs = append(chs, cur)
+	for cur := n.FirstChild; cur != nil; cur = cur.Next {
+		children = append(children, cur)
 	}
-
-	return chs
+	return children
 }
 
-// NewNode returns a struct that fulfills the Node, fmt.String, and
-// yaml.Marshaller/Unmarshaller interfaces.
-func NewNode() *node { return new(node) }
-
-func (n *node) AddChild(ch *node) {
-	ch.parent = n
-	if n.last != nil {
-		n.last.next = ch
-		ch.prev = n.last
-		n.last = ch
+func (n *Node) add(c *Node) {
+	c.Parent = n
+	if n.LastChild != nil {
+		n.LastChild.Next = c
+		c.Prev = n.LastChild
+		n.LastChild = c
 		return
 	}
-	n.first = ch
-	n.last = ch
+	n.FirstChild = c
+	n.LastChild = c
 	return
+}
+
+// Add adds the children sequentially.
+func (n *Node) Add(children ...*Node) {
+	for _, child := range children {
+		n.add(child)
+	}
 }
 
 //func (n *node) InsertBefore(ch *node)
